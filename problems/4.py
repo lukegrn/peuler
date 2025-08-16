a040b69e714d1fcf562b54be5338851cf7a6fe47
Luke Green <me@lukegreen.xyz>
2025-08-15 21:59:26 -0500

    problem 4

---
+++ problems/4.py
@@ -0,0 +1,21 @@
+def is_palidromatic(n):
+    lst = [int(x) for x in str(n)]
+    while len(lst) > 1:
+        if lst[0] != lst[-1]:
+            return False
+
+        lst.pop(0)
+        lst.pop()
+
+    return True
+
+
+def solve():
+    return max(
+        [
+            x * y
+            for x in range(100, 1000)
+            for y in range(100, 1000)
+            if is_palidromatic(x * y)
+        ]
+    )
