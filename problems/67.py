f3d0835159d6102d6b7a0e7ab84eb2763f8f01ec
Luke Green <me@lukegreen.xyz>
2025-08-23 21:34:07 -0500

    18 and 67

---
+++ problems/67.py
@@ -0,0 +1,25 @@
+import os
+
+cache = {}
+
+
+def max_path_value(tree: list[list[int]], x: int = 0, y: int = 0):
+    if f"{x},{y}" not in cache:
+
+        if y == len(tree) - 1:
+            cache[f"{x},{y}"] = tree[y][x]
+        else:
+            cache[f"{x},{y}"] = tree[y][x] + max(
+                [max_path_value(tree, x, y + 1), max_path_value(tree, x + 1, y + 1)]
+            )
+
+    return cache[f"{x},{y}"]
+
+
+def solve():
+    with open(f"{os.path.dirname(__file__)}/files/67.txt") as file:
+        data = file.read()
+
+    tree = [[int(i) for i in line.split(" ")] for line in data.strip().split("\n")]
+
+    return max_path_value(tree)
